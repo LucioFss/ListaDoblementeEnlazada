f2188aeb9e7d63a15ab6a3993fe5812f3e5cd5fd
LucioFassano <fassanolucio06@gmail.com>
2026-02-16 23:15:46 -0300

    Se agregaron los metodos de agregar al principio y final de la lista, y un .txt de anotaciones durante el proceso de desarrollo

--- MyList.java
+++ MyList.java
@@ -5,11 +5,55 @@ public class MyList<T> {
 
     public MyList(){
         this.header = new Node<T>(null);
-        this.trailer = new Node<T>(null);
+        this.trailer = new Node<>(null);
         this.size = 0;
         header.next = trailer;
         trailer.prev = header;
     }
 
-    public getSize()
+    public int size(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    public void addStart(T elem){
+        Node<T> newNode = new Node<>(elem);
+
+        header.next.prev = newNode;
+        newNode.next = header.next;
+        newNode.prev = header;
+        header.next = newNode;
+
+        size++;
+        
+    }
+
+    public void addFinal(T elem){
+        Node<T> newNode = new Node<>(elem);
+        
+        newNode.next = trailer;
+        newNode.prev = trailer.prev;
+        trailer.prev.next = newNode;
+        trailer.prev = newNode;
+        size++;
+    }
+
+    public void printStringStart(){
+        Node<T> aux = header;
+        for(int i = 0; i <= size; i++){
+            aux = aux.next;
+            System.out.print("[" + aux.data + "]");
+        }
+    }
+
+    public void printStringFinal(){
+        Node<T> aux = trailer;
+        for(int i = size; i >= 0; i--){
+            aux = aux.prev;
+            System.out.print("[" + aux.data + "]");
+        }
+    }
 }
---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        MyList<Integer> listaD = new MyList<>();
+
+        listaD.addStart(12);
+        listaD.addStart(13);
+        listaD.addStart(14);
+        listaD.addStart(15);
+        listaD.addStart(16);
+        listaD.addStart(17);
+        listaD.addStart(118);
+        listaD.addStart(119);
+        listaD.printStringStart();
+        listaD.printStringFinal();
+    }
+}
